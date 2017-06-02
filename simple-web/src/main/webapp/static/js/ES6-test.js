df12f8e29b37a5d4f28cd5c62e331d657218a1c2
Abel.shi <abel.shi@derbysoft.com>
2017-06-02 19:09:59 +0800

    ES6 class

--- simple-web/src/main/webapp/static/js/ES6-test.js
+++ simple-web/src/main/webapp/static/js/ES6-test.js
@@ -10,3 +10,30 @@ function stringTemplate() {
 
     var obj = {x: 1, y: 2};     //`${obj.x + obj.y}`;// 3
 }
+
+//模板字符串嵌套
+const tmpl = addrs => `<table>${addrs.map(
+    addr => `<tr><td>${addr.first}</td></tr> 
+             <tr><td>${addr.last}</td></tr>`
+)}</table>`;
+
+const tmplData =  [
+    { first: '<Jane>', last: 'Bond' },
+    { first: 'Lars', last: '<Croft>' },
+];
+console.log(tmpl(tmplData));
+
+export class Person {
+    //noinspection JSAnnotator
+    set name(name) {
+        console.log("set person name:" + name);
+    }
+
+    get name() {
+        console.log("get person name:" + name);
+    }
+}
+
+let person = new Person();
+person.name = 'aaa';
+person.name;
